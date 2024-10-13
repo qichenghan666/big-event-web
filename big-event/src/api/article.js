8e2fd01effd1be29e7c0bab1961a54dddeaa2cae
hanqc <qichenghan@163.com>
2024-10-13 15:03:53 +0800

    大事件前端

---
+++ big-event/src/api/article.js
@@ -0,0 +1,46 @@
+//导入request.js请求工具
+import request from '@/utils/request.js'
+//导入@/stores/token.js
+import { useTokenStore } from '../stores/token'
+
+export const articleCategoryListService = ()=>{
+    // //获取token状态
+    // const tokenStore = useTokenStore()    
+    // return request.get('/category', { headers: { 'Authorization': tokenStore.token } })
+    return request.get('/category')
+}
+
+//添加文章分类
+export const articleCategoryAddService = (categoryModel) => {
+    return request.post('/category', categoryModel)
+}
+
+//修改分类
+export const articleCategoryUpdateService = (categoryModel)=>{
+    return request.put('/category',categoryModel)
+}
+
+
+//删除分类
+export const articleCategoryDeleteService = (id) => {
+    return request.delete('/category?id='+id)
+}
+
+//文章列表查询
+export const articleListService = (params) => {
+    return request.get('/article', { params: params })
+}
+//添加文章
+export const articleAddService = (articleModel)=>{
+    return request.post('/article',articleModel)
+}
+
+
+//修改文章
+export const articleUpdateService = (articleModel)=>{
+    return request.put('/article',articleModel)
+}
+//删除文章
+export const articleDeleteService = (id) => {
+    return request.delete('/article?id='+id)
+}
